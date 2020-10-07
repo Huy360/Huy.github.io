977dd0b1fb8f61f0fe0c166e37c84013cecc5096
Huy360 <71339043+Huy360@users.noreply.github.com>
2020-10-07 15:29:39 -0400

    change

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,7 +1,8 @@
-let myFullName = "Huy Nguyen"; // 9 characters in name...not including space
+let fullName = "Huy Nguyen"; // 9 characters in name...not including space
 
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
-document.querySelector(".header").innerHTML = myFullName + "\'s Lab 4\n";
+document.querySelector(".header").innerHTML = fullName + "\'s Lab 4\n";
+
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
 var newParagraphOne = document.createElement("p1");
@@ -18,25 +19,25 @@ newContentParagraphs.appendChild(newParagraphTwo);
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-var newPhrase = "My name has " + myFullName.length + " characters.";
+var newPhrase = "My name has " + fullName.length + " characters.";
 newParagraphOne.append(newPhrase);
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
-var thirdCharacterInFullName = myFullName.charAt(2);
+var thirdCharacterInFullName = fullName.charAt(2);
 
 // 6 Add a new line to your second paragraph
 newParagraphTwo.append("\n");
 
 // 7 Return the final three characters of your last name to that new line
-var finalThreeCharactersInFullName = myFullName.substring(myFullName.length - 3, myFullName.length);
+var finalThreeCharactersInFullName = fullName.substring(fullName.length - 3, fullName.length);
 newParagraphTwo.append(finalThreeCharactersInFullName);
 
 // 8 Substring your first and last name into two separate variables
-var firstName = myFullName.substring(0, 3);
-var lastName = myFullName.substring(13, 18);
+var firstName = fullName.substring(0, 3);
+var lastName = fullName.substring(13, 18);
 
 // 9 Add the total length of your first and last names together
 var nameLength = firstName.length + lastName.length;
 
 // 10 Display that total next to your name in your header
-document.querySelector(".header").append("The length of my first and last name is: " + nameLength);
+document.querySelector(".header").append("The length of my name is " + nameLength);
