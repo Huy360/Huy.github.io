e9b513fefbc34b7bf70d208f5a5eaad1f2581736
Huy360 <71339043+Huy360@users.noreply.github.com>
2020-11-20 18:06:43 -0500

    update

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -6,13 +6,34 @@ console.log(url);
 let settings = { method: "Get" };
 let chartValues = [];
 
+function randomNum(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
 async function getData() {
     await fetch(url, settings)
         .then(res => res.json())
         .then((json) => {
             let listSize = json.data.children.length;
+            console.log(listSize)
             // Loop to pick 5 random entries
             for (x = 0; x < 5; x++) {
+                let random = randomNum(0, listSize);
+                let post = json.data.children[random].data;
+                console.log(post)
+                let subreddit = post.subreddit;
+                let author = post.author;
+                let title = post.title;
+                let ups = post.ups;
+
+                let message = "<b>Subreddit </b>: " + subreddit + 
+                " <b>Author</b>:" + author + " <b>Title</b>:" + 
+                title + " <b>Up votes</b>: " + ups; 
+
+                let select = document.getElementById("redditList");
+                select.innerHTML += "<li>" + message + "</li>"; 
                 /*
                     Get a random number within the size of the list
                     Get subreddit, author, title, and ups from record
@@ -27,13 +48,14 @@ async function getData() {
                 
                 let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
                 /*.......*/
+                chartValues.push(addToChart); 
             }
         })
         .then(values => console.log(chartValues));
-        //chart.render(); // Do you need to remove the comments from here in order to get it to work?
+        chart.render(); // Do you need to remove the comments from here in order to get it to work?
 };
 
-window.onload = async function makeChart() {
+async function makeChart() {
     getData();
     chart = new CanvasJS.Chart("chartContainer", {
         title: {
@@ -44,7 +66,8 @@ window.onload = async function makeChart() {
             { 
                 type: "column",
                 name: "Popular Reddit",
-                dataPoints: // WHAT GOES HERE???
+                dataPoints: chartValues
+                // WHAT GOES HERE???
             }
         ]
     });
@@ -52,4 +75,4 @@ window.onload = async function makeChart() {
     chart.render();
 }
 
-//window.onload = makeChart();
+window.onload = makeChart();
